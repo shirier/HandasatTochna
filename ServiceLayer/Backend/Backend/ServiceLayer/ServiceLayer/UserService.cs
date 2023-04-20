c3744fb0ea7ab0f6d90d17384fe85ef0abb53526
shirier <shirier@post.bgu.ac.il>
2023-04-20 20:49:20 +0300

    Summed up all of our branches manually because we faces some issues. Here we have all of our code and uml

---
+++ ServiceLayer/Backend/Backend/ServiceLayer/ServiceLayer/UserService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ServiceLayer
+{
+    class UserService
+    {
+
+        private UserFacade userFacade;
+
+        public string register(string email, string password)
+        {
+            return JsonSerializer.Serialize(new Response("Not impleneted yet"));
+        }
+
+        public string login(string email, string password)
+        {
+            return JsonSerializer.Serialize(new Response("Not impleneted yet"));
+        }
+        public string logout(string email)
+        {
+            return JsonSerializer.Serialize(new Response("Not impleneted yet"));
+        }
+
+
+        public string getBoards(string email)
+        {
+            return JsonSerializer.Serialize(new Response("Not impleneted yet"));
+        }
+
+        public string createBoard(string email, string boardname)
+        {
+            return JsonSerializer.Serialize(new Response("Not impleneted yet"));
+        }
+
+        public string deleteBoard(string email, string boardname)
+        {
+            return JsonSerializer.Serialize(new Response("Not impleneted yet"));
+        }
+    }
+}
